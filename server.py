ea0c3a7c18e9e658ac6bc64172dd823fa441cf2e
prakashdev786 <praj78621@gmail.com>
2024-10-29 12:31:26 +0530

    port change

--- server.py
+++ server.py
@@ -10,5 +10,5 @@ app = create_app(os.getenv("CONFIG_MODE"))
 if __name__ == "__main__":
     # To Run the Server in Terminal => flask run -h localhost -p 5000
     # To Run the Server with Automatic Restart When Changes Occurred => FLASK_DEBUG=1 flask run -h localhost -p 5000
-    app.run()
+    app.run(host='0.0.0.0', port=10000)
     
